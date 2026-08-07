1011d5ba9b8612fa49b3b041123dfc187487f704
lalv556 <74160726+lalv556@users.noreply.github.com>
2026-08-08 00:18:27 +0200

    Per n and function evaluator to analyse latency

--- run_evaluator.py
+++ run_evaluator.py
@@ -1,7 +1,8 @@
 import cProfile
 import time
 
-import generator
+import array_generator
+import set_generator
 
 def time_analysis():
     if per_function_analysis:
@@ -35,9 +36,10 @@ def time_analysis():
         print(f"{n}: {time.time() - start:.3e} seconds")
 
 n = 9
+generator = set_generator
 use_saved = False
 per_function_analysis = False
-iterator = False
+iterator = True
 
 if iterator:
     for i in range(1, n+1):
@@ -47,6 +49,7 @@ else:
     time_analysis()
 
 """
+Array generator
 1: 1.431e-06 seconds
 2: 9.537e-06 seconds
 3: 2.403e-04 seconds
@@ -56,4 +59,15 @@ else:
 7: 8.523e-01 seconds
 8: 1.028e+01 seconds
 9: 2.246e+02 seconds
+
+Set generator
+1: 1.907e-06 seconds
+2: 2.384e-06 seconds
+3: 1.318e-04 seconds
+4: 3.777e-04 seconds
+5: 1.489e-03 seconds
+6: 5.111e-03 seconds
+7: 2.070e-02 seconds
+8: 7.458e-02 seconds
+9: 2.944e-01 seconds
 """
